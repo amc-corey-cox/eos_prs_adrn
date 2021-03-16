b8a52d5d0c95c531245863cff1506cd0410af903
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2021-03-16 05:03:11 +0000

    Fix Tabix Scripts on Eureka

--- 1_create_tabix.sh
+++ 1_create_tabix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p c2s4
-#SBATCH --job-name=hpc_tmp
-#SBATCH --out=hpc_tmp.log
-#SBATCH --error=hpc_tmp.err
+#SBATCH --job-name=EA_O_tab
+#SBATCH --out=EA_O_tab.log
+#SBATCH --error=EA_O_tab.err
 
 # These lines generally make bash shell scripts safer
 set -euo pipefail        # -e: exit on any error, -u: treat unset variables as error
@@ -24,11 +24,11 @@ PATH=$tmp_dir/:$PATH
 mkdir -p gcs_mnt/hdcekabarnesadrn1/
 gcsfuse --implicit-dirs hdcekabarnesadrn1 gcs_mnt/hdcekabarnesadrn1/
 
-base_dir=gcs_mnt/hdcekabarnesadrn1/ADRN_MEGA_META_SEVERITY/b_EA_MEGA/imputed
+base_dir=gcs_mnt/hdcekabarnesadrn1/ADRN_MEGA_META_SEVERITY/b_EA_OMNI/imputed
 for i in {22..1}; do
   if [[ -f "${base_dir}/chr${i}.dose.vcf.gz.tbi" ]]; then continue; fi
   echo tabix -f "${base_dir}/chr${i}.dose.vcf.gz"
-  tabix -f "${base_dir}/imputed/chr${i}.dose.vcf.gz"
+  tabix -f "${base_dir}/chr${i}.dose.vcf.gz"
 done
 
-
+# log files moved to ~/gcs_mnt/hdcekabarnesadrn1/ADRN_MEGA_META_SEVERITY/EOS_PRS/log
--- 2_extract_snps.sh
+++ 2_extract_snps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#SBATCH -p c2s4
+#SBATCH --job-name=eos_prs_ext
+#SBATCH --out=eos_prs_ext.log
+#SBATCH --error=eos_prs_ext.err
+
 # These lines generally make bash shell scripts safer
 set -euo pipefail        # -e: exit on any error, -u: treat unset variables as error
 IFS="`printf '\n\t'`"    # split words only on \n and \t, not space (improves loops)
@@ -6,10 +11,26 @@ IFS="`printf '\n\t'`"    # split words only on \n and \t, not space (improves lo
 # Uncomment this for better logging
 set -x # Print each command after variable exansion
 
+tmp_dir=$(mktemp -d)
+cd "${tmp_dir}"
+
+
+data_gs=hdcekabarnesadrn1/ADRN_MEGA_META_SEVERITY/EOS_PRS
+gsutil -q cp "gs://${data_gs}/eos_prs_snps_hg38.txt" .
+gsutil -q cp "gs://${data_gs}/tools/bin/tabix" .
+chmod +x tabix
+
+PATH=$tmp_dir/:$PATH
+
+
+mkdir -p gcs_mnt/hdcekabarnesadrn1/
+gcsfuse --implicit-dirs hdcekabarnesadrn1 gcs_mnt/hdcekabarnesadrn1/
+
+rm -f eos_prs_snps_hg38_extracted.vcf
 for i in {1..22}; do
-  awk -F '[:_\t]' -v chr="^$i" '$0 ~ chr { print $1 ":" $2 "-" $2 }' \
-    BCX_trans_ethnic_risk_scores/beta_scoring_Trans_EOS.bed.lifted.hg38 \
-    > extract.tmp
-    tabix -T extract.tmp /gcs_mnt/hdcekabarnesadrn1/ADRN_MEGA_META_SEVERITY/b_AA/imputed/chr$i.dose.vcf.gz > tmp.vcf
+  awk -F '[:_\t]' -v chr="^$i" '$0 ~ chr { print "chr" $1 "\t" $2 "\t" $2 }' \
+    eos_prs_snps_hg38.txt > extract.tmp
+    tabix -R extract.tmp gcs_mnt/hdcekabarnesadrn1/ADRN_MEGA_META_SEVERITY/b_AA/imputed/chr$i.dose.vcf.gz >> eos_prs_snps_hg38_extracted.vcf
 done
-  
+
+gsutil -q cp eos_prs_snps_hg38_extracted.vcf "gs://${data_gs}/"
